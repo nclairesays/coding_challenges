d9b829197477d1cd66588f3fffb82594abb9541a
nclairesays <45305029+nclairesays@users.noreply.github.com>
2019-07-13 15:37:06 -0700

    updated html/answer

--- cox/client/index.js
+++ cox/client/index.js
@@ -1,55 +1,69 @@
 const api = 'http://api.coxauto-interview.com/api'
 
-const fetchDataID = async () => {
+const getDataID = async () => {
     return await fetch(`${api}/datasetId`)
     .then(res => res.json())
     .then(res => res.datasetId)
 }
 
-
-// GET /api/{datasetId}/vehicles  |  Get a list of all vehicleids in datbase
-const fetchVehicleIDs = async (datasetId) => {
-    // await fetchDataID()
+const getVehicleIDs = async (datasetId) => {
     return await fetch(`${api}/${datasetId}/vehicles`)
     .then(res => res.json())
     .then(res => res.vehicleIds)
 }
 
-const fetchVehicleInfo = async (datasetId, vehicleIds) => {
+const getVehicleInfo = async (datasetId, vehicleIds) => {
     return await fetch(`${api}/${datasetId}/vehicles/${vehicleIds}`)
     .then(res => res.json())
 }
 
-// GET /api/{datasetId}/dealers/{dealerid} Get information about a dealer
-const fetchDealerInfo = async (datasetId, dealerId) => {
+const getDealerInfo = async (datasetId, dealerId) => {
     return await fetch(`${api}/${datasetId}/dealers/${dealerId}`)
     .then(res => res.json())
     .then(res => res.name)
 }
 
 const postAnswers = async (datasetId, dealers) => {
-    // const data = dealers.
-    console.log(dealers)
+    const data = []
+    for (let dealer in dealers) {
+        data.push(dealers[dealer])
+    }
+    // ANSWER LOGGED:
+    console.log("MY ANSWER/PAYLOAD/BODY:", JSON.stringify({"dealers": data }))
+    
+    renderOnPage(datasetId, data)
+
     return await fetch(`${api}/${datasetId}/answer`, {
-        method: 'POST',
-        mode: 'no-cors', // no-cors, cors, *same-origin
+        method: "POST",
+        // I am getting CORS errors. But when I add the mode: no-cors, it gives me a Media Type error. 
+        // mode: "no-cors", 
         headers: {
-            'Content-Type': 'application/json',
+            "content-type": "application/json",
+            "accept": "application/json",
         },
-        body: JSON.stringify(dealers)
+        body: JSON.stringify({ "dealers": data })
     })
+    .then(res => res.json())    
+    // .then(res => console.log(res))
 }
-const allTheFetches = async () => {
+
+const renderOnPage = (datasetId, data) => {
+    document.querySelector('#answer').innerHTML = `DatasetId:${datasetId} <br><br>` + JSON.stringify({"dealers": data})
+}
+ 
+
+const render = async () => {
     try {
         let dealers = {}
-        const datasetId = await fetchDataID()
-        const vehicleIds = await fetchVehicleIDs(datasetId)
+        const datasetId = await getDataID()
+        console.log("datasetId", datasetId)
+        const vehicleIds = await getVehicleIDs(datasetId)
         await Promise.all(
             vehicleIds.map( async vehicleID => {
-                await fetchVehicleInfo(datasetId, vehicleID)
+                await getVehicleInfo(datasetId, vehicleID)
                 .then( async res => {
                     const { vehicleId, year, make, model, dealerId} = res
-                    const dealerName = await fetchDealerInfo(datasetId, dealerId)
+                    const dealerName = await getDealerInfo(datasetId, dealerId)
                     if (dealers[dealerId]) {
                         dealers[dealerId].vehicles.push({
                         "vehicleId": vehicleId,
@@ -78,139 +92,5 @@ const allTheFetches = async () => {
         console.log(error.message)
     }
 }
-allTheFetches()
 
-
-
-
-
-  // .then(res => {   
-        //     return dealers[res.dealerId] = res
-        // })
-        // .then( res => console.log(dealers))
-        // .then(res => {
-        //     console.log(res)
-        //     return dealersSet.add(res.dealerId)
-        // })
-        // .then( res => console.log(dealers))
-        // .then(res => fetchDealerInfo(datasetId, res.dealerId))
-        // .then(res => console.log("FETCHED DEALER, Dataset: ", datasetId, "DEALER", res) )
-
-
-        
-        // const getDealers
-
-        // const check = () => {
-        //     if(dealers.datasetId)
-        // }
-
-        // fetch('http://api.coxauto-interview.com/api/4-h8emQH1wg/dealers/1577899834')
-        // .then(res => res.json()).then(res => console.log(res))
-
-        // "dealers": [
-            //       {
-            //         "dealerId": 1279401307,
-            //         "name": "Bob's Cars",
-            //   dealers": [
-            //           {
-            //             "vehicleId": 72059268,
-            //             "year": 2016,
-            //             "make": "Honda",
-            //             "model": "Accord"
-            //           },
-
-
-        // await console.log("INFO", vehicleInfo)
-
-        // return await vehicleIds 
-
-
-
-
-// "vehicleId": 0,
-//     "year": 0,
-//     "make": "string",
-//     "model": "string",
-//     "dealerId": 0
-
-
-
-// {
-//     "dealers": [
-//       {
-//         "dealerId": 1279401307,
-//         "name": "Bob's Cars",
-//   dealers": [
-//           {
-//             "vehicleId": 72059268,
-//             "year": 2016,
-//             "make": "Honda",
-//             "model": "Accord"
-//           },
-//           {
-//             "vehicleId": 841872961,
-//             "year": 2014,
-//             "make": "Ford",
-//             "model": "F150"
-//           },
-//           {
-//             "vehicleId": 722905753,
-//             "year": 2004,
-//             "make": "MINI",
-//             "model": "Cooper"
-//           }
-//         ]
-//       },
-//       {
-//         "dealerId": 608903662,
-//         "name": "House of Wheels",
-//         "vehicles": [
-//           {
-//             "vehicleId": 1911234494,
-//             "year": 2013,
-//             "make": "Mitsubishi",
-//             "model": "Gallant"
-//           },
-//           {
-//             "vehicleId": 386858413,
-//             "year": 2009,
-//             "make": "Ford",
-//             "model": "F150"
-//           },
-//           {
-//             "vehicleId": 60320518,
-//             "year": 2016,
-//             "make": "Kia",
-//             "model": "Soul"
-//           }
-//         ]
-//       },
-//       {
-//         "dealerId": 575943453,
-//         "name": "Doug's Doozies",
-//         "vehicles": [
-//           {
-//             "vehicleId": 1182728774,
-//             "year": 2016,
-//             "make": "Bentley",
-//             "model": "Mulsanne"
-//           },
-//           {
-//             "vehicleId": 1676468526,
-//             "year": 2012,
-//             "make": "Nissan",
-//             "model": "Altima"
-//           },
-//           {
-//             "vehicleId": 105937686,
-//             "year": 1979,
-//             "make": "Cheverolet",
-//             "model": "Corvette"
-//           }
-//         ]
-//       }
-//     ]
-//   }
-//   Response Code
-//   200
-//   Response Headers
+render()
